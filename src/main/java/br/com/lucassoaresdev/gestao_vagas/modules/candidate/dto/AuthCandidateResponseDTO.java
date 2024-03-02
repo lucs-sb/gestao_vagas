b36496aea0bc56255098a5396fc5ea917cd667da
Lucas Soares <soares0lucas8@gmail.com>
2024-03-02 08:42:18 -0300

    refactor: criando token para autenticação do candidato

---
+++ src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,4 @@
+package br.com.lucassoaresdev.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateResponseDTO(String access_token) {
+}
---
+++ src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,51 @@
+package br.com.lucassoaresdev.gestao_vagas.modules.candidate.useCases;
+
+import br.com.lucassoaresdev.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.lucassoaresdev.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.lucassoaresdev.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.lucassoaresdev.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Optional;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Value("${security.token.secret.candidate}")
+    private String secretKey;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+        Optional<CandidateEntity> candidate = Optional.ofNullable(this.candidateRepository.findByUsername(authCandidateRequestDTO.username())
+                .orElseThrow(() -> new UsernameNotFoundException("Username/password incorrect")));
+
+        boolean passwordMatches = this.passwordEncoder.matches(authCandidateRequestDTO.password(), candidate.get().getPassword());
+
+        if (!passwordMatches)
+            throw new AuthenticationException();
+
+        Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+
+        String token = JWT.create().withIssuer("javagas")
+                .withSubject(candidate.get().getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+                .sign(algorithm);
+
+        return new AuthCandidateResponseDTO(token);
+    }
+}
