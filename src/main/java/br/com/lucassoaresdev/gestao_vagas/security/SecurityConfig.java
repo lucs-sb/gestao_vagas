2c22068ea4ccc0236460f2539604367e3a1ee528
Lucas Soares <soares0lucas8@gmail.com>
2024-03-16 10:43:39 -0300

    feat: habilitando documentação com o swagger

--- src/main/java/br/com/lucassoaresdev/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/lucassoaresdev/gestao_vagas/security/SecurityConfig.java
@@ -21,14 +21,22 @@ public class SecurityConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                        auth.requestMatchers("/candidate").permitAll()
+                        auth
+                                .requestMatchers("/candidate").permitAll()
+                                .requestMatchers("/candidate/auth").permitAll()
                                 .requestMatchers("/company").permitAll()
                                 .requestMatchers("/company/auth").permitAll()
-                                .requestMatchers("/candidate/auth").permitAll();
+                                .requestMatchers(SWAGGER_LIST).permitAll();
                         auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(this.securityCandidateFilter, BasicAuthenticationFilter.class)
