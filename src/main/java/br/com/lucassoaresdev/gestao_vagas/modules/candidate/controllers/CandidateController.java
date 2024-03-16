35b0267069c50bff5a27544e0fe26f5b445afc09
Lucas Soares <soares0lucas8@gmail.com>
2024-03-16 10:53:06 -0300

    feat: endpoint para o candidato buscar jobs

--- src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,7 +3,9 @@ package br.com.lucassoaresdev.gestao_vagas.modules.candidate.controllers;
 import br.com.lucassoaresdev.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.lucassoaresdev.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.lucassoaresdev.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.lucassoaresdev.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.lucassoaresdev.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.lucassoaresdev.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,6 +25,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidate) {
@@ -45,5 +51,11 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter){
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
--- src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/lucassoaresdev/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -3,7 +3,10 @@ package br.com.lucassoaresdev.gestao_vagas.modules.company.repositories;
 import br.com.lucassoaresdev.gestao_vagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    List<JobEntity> findByDescriptionContaining(String filter);
 }
